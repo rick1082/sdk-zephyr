31674dcd723dc61c4e350c485f3da4cdf30eead6
Jui-Chou Chung <jui-chou.chung@nordicsemi.no>
2023-01-30 16:27:39 +0800

    Initial commit for low latency dual CIS demo

--- samples/bluetooth/central_iso/src/main.c
+++ samples/bluetooth/central_iso/src/main.c
@@ -8,7 +8,6 @@
 #include <stddef.h>
 #include <errno.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
@@ -18,16 +17,128 @@
 #include <zephyr/bluetooth/iso.h>
 #include <zephyr/settings/settings.h>
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(ble);
 
 static void start_scan(void);
 
-static struct bt_conn *default_conn;
+#define DEVICE_NAME_PEER_L     "Peripheral_L"
+#define DEVICE_NAME_PEER_R     "Peripheral_R"
+#define DEVICE_NAME_PEER_L_LEN (sizeof(DEVICE_NAME_PEER_L) - 1)
+#define DEVICE_NAME_PEER_R_LEN (sizeof(DEVICE_NAME_PEER_R) - 1)
+#define CIS_CONN_RETRY_TIMES   5
+#define ISO_MAX_PAYLOAD 30
+#define BT_LE_CONN_PARAM_MULTI BT_LE_CONN_PARAM(40, 40, 0, 400)
+
+static struct bt_conn *gateway_conn_peer[CONFIG_BT_MAX_CONN];
 static struct k_work_delayable iso_send_work;
-static struct bt_iso_chan iso_chan;
-static uint16_t seq_num;
-static uint32_t interval_us = 10U * USEC_PER_MSEC; /* 10 ms */
-NET_BUF_POOL_FIXED_DEFINE(tx_pool, 1, BT_ISO_SDU_BUF_SIZE(CONFIG_BT_ISO_TX_MTU), 8,
-			  NULL);
+
+static struct bt_iso_chan iso_chan[CONFIG_BT_ISO_MAX_CHAN];
+static struct bt_iso_chan *iso_chan_p[CONFIG_BT_ISO_MAX_CHAN];
+static struct k_work_delayable iso_cis_conn_work;
+
+#define ISO_INTERVAL_US 5000//(5U * USEC_PER_MSEC) /* 10 ms */
+
+struct worker_data {
+	uint8_t channel;
+	uint8_t retries;
+} __aligned(4);
+
+K_MSGQ_DEFINE(kwork_msgq, sizeof(struct worker_data), CONFIG_BT_ISO_MAX_CHAN, 4);
+
+int ble_acl_gateway_conn_peer_get(uint8_t chan_number, struct bt_conn **p_conn)
+{
+	if (chan_number >= CONFIG_BT_MAX_CONN) {
+		return -EINVAL;
+	}
+	*p_conn = gateway_conn_peer[chan_number];
+	return 0;
+}
+
+int ble_acl_gateway_conn_peer_set(uint8_t chan_number, struct bt_conn **p_conn)
+{
+	if (chan_number >= CONFIG_BT_MAX_CONN) {
+		return -EINVAL;
+	}
+
+	if (gateway_conn_peer[chan_number] != NULL) {
+		if (*p_conn == NULL) {
+			gateway_conn_peer[chan_number] = NULL;
+		} else {
+			LOG_WRN("Already have a connection for peer: %d", chan_number);
+		}
+		/* Ignore duplicates as several peripherals might be
+		 * advertising at the same time
+		 */
+		return 0;
+	}
+
+	gateway_conn_peer[chan_number] = *p_conn;
+	return 0;
+}
+
+bool ble_acl_gateway_all_links_connected(void)
+{
+	for (int i = 0; i < CONFIG_BT_MAX_CONN; i++) {
+		if (gateway_conn_peer[i] == NULL) {
+			return false;
+		}
+	}
+	return true;
+}
+
+static void work_iso_cis_conn(struct k_work *work)
+{
+	int ret;
+	struct bt_iso_connect_param connect_param;
+	struct worker_data work_data;
+
+	ret = k_msgq_get(&kwork_msgq, &work_data, K_NO_WAIT);
+	if (ret) {
+		LOG_ERR("k_msgq_get failed");
+	}
+
+	ret = ble_acl_gateway_conn_peer_get(work_data.channel, &connect_param.acl);
+	if (ret) {
+		LOG_ERR("Connection peer get error");
+	}
+	connect_param.iso_chan = iso_chan_p[work_data.channel];
+
+	ret = bt_iso_chan_connect(&connect_param, 1);
+	work_data.retries++;
+	if (ret) {
+		if (work_data.retries < CIS_CONN_RETRY_TIMES) {
+			LOG_WRN("Got connect error from ch %d Retrying. code: %d count: %d",
+				work_data.channel, ret, work_data.retries);
+			ret = k_msgq_put(&kwork_msgq, &work_data, K_NO_WAIT);
+			if (ret) {
+				LOG_ERR("k_msgq_put failed");
+			}
+			/* Delay added to prevent controller overloading */
+			k_work_reschedule(&iso_cis_conn_work, K_MSEC(500));
+		} else {
+			LOG_ERR("Could not connect ch %d after %d retries", work_data.channel,
+				work_data.retries);
+			bt_conn_disconnect(connect_param.acl, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+		}
+	}
+}
+static uint8_t iso_chan_to_idx(struct bt_iso_chan *chan)
+{
+	if (chan == NULL) {
+		LOG_ERR("chan is NULL");
+	}
+
+	for (uint8_t i = 0; i < CONFIG_BT_ISO_MAX_CHAN; i++) {
+		if (chan == iso_chan_p[i]) {
+			return i;
+		}
+	}
+
+	LOG_ERR("No index found for this channel");
+	CODE_UNREACHABLE;
+	return UINT8_MAX;
+}
 
 /**
  * @brief Send ISO data on timeout
@@ -44,154 +155,195 @@ NET_BUF_POOL_FIXED_DEFINE(tx_pool, 1, BT_ISO_SDU_BUF_SIZE(CONFIG_BT_ISO_TX_MTU),
  */
 static void iso_timer_timeout(struct k_work *work)
 {
-	int ret;
-	static uint8_t buf_data[CONFIG_BT_ISO_TX_MTU];
-	static bool data_initialized;
-	struct net_buf *buf;
-	static size_t len_to_send = 1;
 
-	if (!data_initialized) {
-		for (int i = 0; i < ARRAY_SIZE(buf_data); i++) {
-			buf_data[i] = (uint8_t)i;
+}
+
+static int device_found(uint8_t type, const uint8_t *data, uint8_t data_len,
+			const bt_addr_le_t *addr)
+{
+	int ret;
+	struct bt_conn *conn;
+
+	if ((data_len == DEVICE_NAME_PEER_L_LEN) &&
+	    (strncmp(DEVICE_NAME_PEER_L, data, DEVICE_NAME_PEER_L_LEN) == 0)) {
+
+		ret = bt_le_scan_stop();
+		if (ret) {
+			LOG_INF("Stop scan failed: %d", ret);
 		}
 
-		data_initialized = true;
+		ret = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_MULTI,
+					&conn);
+		if (ret) {
+			LOG_INF("Could not init connection");
+			return ret;
+		}
+		ret = ble_acl_gateway_conn_peer_set(0, &conn);
+		if (ret) {
+			LOG_ERR("Connection peer set error %d", ret);
+		}
+		return 0;
+	} else if ((data_len == DEVICE_NAME_PEER_R_LEN) &&
+		   (strncmp(DEVICE_NAME_PEER_R, data, DEVICE_NAME_PEER_R_LEN) == 0)) {
+		ret = bt_le_scan_stop();
+		if (ret) {
+			LOG_ERR("Stop scan failed: %d", ret);
+		}
+
+		ret = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_MULTI,
+					&conn);
+		if (ret) {
+			LOG_INF("Could not init connection");
+			return ret;
+		}
+		ret = ble_acl_gateway_conn_peer_set(1, &conn);
+		if (ret) {
+			LOG_ERR("Connection peer set error %d", ret);
+		}
+		return 0;
 	}
 
-	buf = net_buf_alloc(&tx_pool, K_FOREVER);
-	net_buf_reserve(buf, BT_ISO_CHAN_SEND_RESERVE);
-
-	net_buf_add_mem(buf, buf_data, len_to_send);
-
-	ret = bt_iso_chan_send(&iso_chan, buf, seq_num++, BT_ISO_TIMESTAMP_NONE);
-
-	if (ret < 0) {
-		printk("Failed to send ISO data (%d)\n", ret);
-	}
-
-	k_work_schedule(&iso_send_work, K_USEC(interval_us));
-
-	len_to_send++;
-	if (len_to_send > ARRAY_SIZE(buf_data)) {
-		len_to_send = 1;
-	}
+	return -ENOENT;
 }
 
-static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
-			 struct net_buf_simple *ad)
+/** @brief  Parse BLE advertisement package.
+ */
+static void ad_parse(struct net_buf_simple *p_ad, const bt_addr_le_t *addr)
 {
-	char addr_str[BT_ADDR_LE_STR_LEN];
-	int err;
+	while (p_ad->len > 1) {
+		uint8_t len = net_buf_simple_pull_u8(p_ad);
+		uint8_t type;
 
-	if (default_conn) {
-		/* Already connected */
-		return;
-	}
+		/* Check for early termination */
+		if (len == 0) {
+			return;
+		}
 
-	/* We're only interested in connectable events */
-	if (type != BT_GAP_ADV_TYPE_ADV_IND && type != BT_GAP_ADV_TYPE_ADV_DIRECT_IND) {
-		return;
-	}
+		if (len > p_ad->len) {
+			LOG_ERR("AD malformed");
+			return;
+		}
 
-	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
-	printk("Device found: %s (RSSI %d)\n", addr_str, rssi);
+		type = net_buf_simple_pull_u8(p_ad);
 
-	/* connect only to devices in close proximity */
-	if (rssi < -70) {
-		return;
-	}
+		if (device_found(type, p_ad->data, len - 1, addr) == 0) {
+			return;
+		}
 
-	if (bt_le_scan_stop()) {
-		return;
-	}
-
-	err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, BT_LE_CONN_PARAM_DEFAULT,
-				&default_conn);
-	if (err) {
-		printk("Create conn to %s failed (%u)\n", addr_str, err);
-		start_scan();
+		(void)net_buf_simple_pull(p_ad, len - 1);
 	}
 }
+
+static void on_device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
+			    struct net_buf_simple *p_ad)
+{
+	/* Direct advertising has no payload, so no need to parse */
+	if (type == BT_GAP_ADV_TYPE_ADV_IND || type == BT_GAP_ADV_TYPE_SCAN_RSP ||
+	    type == BT_GAP_ADV_TYPE_EXT_ADV) {
+		/* Note: May lead to connection creation */
+		ad_parse(p_ad, addr);
+	}
+}
+
 
 static void start_scan(void)
 {
 	int err;
 
 	/* This demo doesn't require active scan */
-	err = bt_le_scan_start(BT_LE_SCAN_PASSIVE, device_found);
+	err = bt_le_scan_start(BT_LE_SCAN_PASSIVE, on_device_found);
 	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
+		LOG_ERR("Scanning failed to start (err %d)", err);
 		return;
 	}
 
-	printk("Scanning successfully started\n");
+	LOG_INF("Scanning successfully started");
+}
+
+static void iso_recv(struct bt_iso_chan *chan, const struct bt_iso_recv_info *info,
+		struct net_buf *buf)
+{
+	LOG_INF("Incoming data channel %p len %u\n", chan, buf->len);
+	//iso_print_data(buf->data, buf->len);
 }
 
 static void iso_connected(struct bt_iso_chan *chan)
 {
-	printk("ISO Channel %p connected\n", chan);
+	LOG_INF("ISO Channel %p connected", chan);
+	uint8_t chan_num;
+	chan_num = iso_chan_to_idx(chan);
 
-	seq_num = 0U;
-
-	/* Start send timer */
-	k_work_schedule(&iso_send_work, K_MSEC(0));
+	LOG_INF("chan %d connected", chan_num);
 }
 
 static void iso_disconnected(struct bt_iso_chan *chan, uint8_t reason)
 {
-	printk("ISO Channel %p disconnected (reason 0x%02x)\n", chan, reason);
+	LOG_INF("ISO Channel %p disconnected (reason 0x%02x)", chan, reason);
 	k_work_cancel_delayable(&iso_send_work);
 }
 
 static struct bt_iso_chan_ops iso_ops = {
+	.recv		= iso_recv,
 	.connected	= iso_connected,
 	.disconnected	= iso_disconnected,
 };
 
-static struct bt_iso_chan_io_qos iso_tx = {
+int ble_trans_iso_cis_connect(struct bt_conn *conn)
+{
+	int ret;
+	struct bt_conn *conn_active;
+
+	for (uint8_t i = 0; i < CONFIG_BT_MAX_CONN; i++) {
+		ret = ble_acl_gateway_conn_peer_get(i, &conn_active);
+		if (ret) {
+			LOG_ERR("Connection peer get error");
+		}
+		if (conn == conn_active) {
+			struct worker_data work_data;
+
+			work_data.channel = i;
+			work_data.retries = 0;
+
+			ret = k_msgq_put(&kwork_msgq, &work_data, K_NO_WAIT);
+			if (ret) {
+				return ret;
+			}
+
+			k_work_schedule(&iso_cis_conn_work, K_MSEC(500 * i));
+		}
+	}
+
+	return 0;
+}
+
+static struct bt_iso_chan_io_qos iso_rx = {
 	.sdu = CONFIG_BT_ISO_TX_MTU,
 	.phy = BT_GAP_LE_PHY_2M,
 	.rtn = 1,
 	.path = NULL,
 };
 
-static struct bt_iso_chan_qos iso_qos = {
-	.tx = &iso_tx,
-	.rx = NULL,
+static struct bt_iso_chan_qos iso_cis_qos = {
+	.tx = NULL,
+	.rx = &iso_rx,
 };
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
-	int iso_err;
-	struct bt_iso_connect_param connect_param;
+	int ret;
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	if (err) {
-		printk("Failed to connect to %s (%u)\n", addr, err);
-
-		bt_conn_unref(default_conn);
-		default_conn = NULL;
-
-		start_scan();
-		return;
+		LOG_ERR("Failed to connect to %s (%u)", addr, err);
 	}
+	LOG_INF("Connected: %s", addr);
 
-	if (conn != default_conn) {
-		return;
-	}
 
-	printk("Connected: %s\n", addr);
-
-	connect_param.acl = conn;
-	connect_param.iso_chan = &iso_chan;
-
-	iso_err = bt_iso_chan_connect(&connect_param, 1);
-
-	if (iso_err) {
-		printk("Failed to connect iso (%d)\n", iso_err);
-		return;
+	ret = bt_conn_set_security(conn, BT_SECURITY_L2);
+	if(ret) {
+		LOG_ERR("set security failed %d", ret);
 	}
 }
 
@@ -199,35 +351,96 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
 
-	if (conn != default_conn) {
-		return;
-	}
-
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
+	struct bt_conn *conn_active;
 
-	bt_conn_unref(default_conn);
-	default_conn = NULL;
+	for (int i = 0; i < CONFIG_BT_MAX_CONN; i++) {
+		int ret;
+
+		ret = ble_acl_gateway_conn_peer_get(i, &conn_active);
+		if (ret) {
+			LOG_ERR("Connection peer get error");
+		}
+		if (conn_active == conn) {
+			bt_conn_unref(conn_active);
+			conn_active = NULL;
+			ret = ble_acl_gateway_conn_peer_set(i, &conn_active);
+			if (ret) {
+				LOG_ERR("Connection peer get error %d", ret);
+			}
+			LOG_DBG("Headset %d disconnected", i);
+			break;
+		}
+	}
 
 	start_scan();
+}
+
+static void security_changed_cb(struct bt_conn *conn, bt_security_t level, enum bt_security_err err)
+{
+	int ret;
+
+	if (err) {
+		LOG_ERR("MTU exchange failed, err = %d", err);
+		ret = bt_conn_disconnect(conn, BT_HCI_ERR_LOCALHOST_TERM_CONN);
+		if (ret) {
+			LOG_ERR("Failed to disconnected, %d", ret);
+		}
+	} else {
+		LOG_DBG("MTU exchange success");
+		if (!ble_acl_gateway_all_links_connected()) {
+			start_scan();
+		} else {
+			LOG_INF("All ACL links are connected");
+			bt_le_scan_stop();
+		}
+
+		ret = ble_trans_iso_cis_connect(conn);
+		if(ret) {
+			LOG_ERR("Failed to connect to ISO CIS channel");
+		}
+	}
 }
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.connected = connected,
 	.disconnected = disconnected,
+	.security_changed = security_changed_cb,
 };
+
+static struct bt_iso_cig_param cis_create_param = {
+	.cis_channels = iso_chan_p,
+	.num_cis = 2,
+	.sca = BT_GAP_SCA_UNKNOWN,
+	.packing = BT_ISO_PACKING_SEQUENTIAL,
+	.framing = BT_ISO_FRAMING_UNFRAMED,
+	.latency = 10,
+	.interval = ISO_INTERVAL_US,
+};
+
+int ble_trans_iso_cig_create(void)
+{
+	int ret;
+
+	struct bt_iso_cig *cig;
+
+	ret = bt_iso_cig_create(&cis_create_param, &cig);
+	if (ret) {
+		LOG_ERR("Failed to create CIG (%d)\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
 
 void main(void)
 {
 	int err;
-	struct bt_iso_chan *channels[1];
-	struct bt_iso_cig_param param;
-	struct bt_iso_cig *cig;
 
 	err = bt_enable(NULL);
 	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+		LOG_ERR("Bluetooth init failed (err %d)", err);
 		return;
 	}
 
@@ -235,32 +448,24 @@ void main(void)
 		settings_load();
 	}
 
+	LOG_INF("Bluetooth initialized");
 
-	printk("Bluetooth initialized\n");
-
-	iso_chan.ops = &iso_ops;
-	iso_chan.qos = &iso_qos;
-#if defined(CONFIG_BT_SMP)
-	iso_chan.required_sec_level = BT_SECURITY_L2,
-#endif /* CONFIG_BT_SMP */
-
-	channels[0] = &iso_chan;
-	param.cis_channels = channels;
-	param.num_cis = ARRAY_SIZE(channels);
-	param.sca = BT_GAP_SCA_UNKNOWN;
-	param.packing = 0;
-	param.framing = 0;
-	param.latency = 10; /* ms */
-	param.interval = interval_us; /* us */
-
-	err = bt_iso_cig_create(&param, &cig);
-
-	if (err != 0) {
-		printk("Failed to create CIG (%d)\n", err);
-		return;
+	for (int8_t i = 0; i < CONFIG_BT_ISO_MAX_CHAN; i++) {
+		iso_chan_p[i] = &iso_chan[i];
 	}
 
-	start_scan();
+	for (int i = 0; i < CONFIG_BT_ISO_MAX_CHAN; i++) {
+		iso_chan_p[i]->ops = &iso_ops;
+		iso_chan_p[i]->qos = &iso_cis_qos;
+	}
 
+	err = ble_trans_iso_cig_create();
+	if (err) {
+		LOG_ERR("CIG create failed %d", err);
+	}
+
+	k_work_init_delayable(&iso_cis_conn_work, work_iso_cis_conn);
 	k_work_init_delayable(&iso_send_work, iso_timer_timeout);
+
+	start_scan();
 }
--- samples/bluetooth/peripheral_iso/src/main.c
+++ samples/bluetooth/peripheral_iso/src/main.c
@@ -8,7 +8,6 @@
 #include <stddef.h>
 #include <string.h>
 #include <errno.h>
-#include <zephyr/sys/printk.h>
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/kernel.h>
 
@@ -17,10 +16,64 @@
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/iso.h>
 #include <zephyr/settings/settings.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(ble);
 
+#define IS_LEFT_PERIPHERAL 0
+
+#if IS_LEFT_PERIPHERAL
+#define DEVICE_NAME             "Peripheral_L"
+#else
+#define DEVICE_NAME             "Peripheral_R"
+#endif
+
+#define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 };
+static struct k_work_delayable iso_send_work;
+static struct bt_iso_chan iso_chan;
+static uint16_t seq_num;
+static atomic_t iso_tx_pool_alloc;
+NET_BUF_POOL_FIXED_DEFINE(tx_pool, 1, BT_ISO_SDU_BUF_SIZE(CONFIG_BT_ISO_TX_MTU), 8,
+			  NULL);
+#define HCI_ISO_BUF_ALLOC_PER_CHAN 2
+
+static void iso_timer_timeout(struct k_work *work)
+{
+	int ret;
+	static uint8_t buf_data[CONFIG_BT_ISO_TX_MTU];
+	struct net_buf *buf;
+	static size_t len_to_send = CONFIG_BT_ISO_TX_MTU;
+	static bool hci_wrn_printed;
+
+	if (atomic_get(&iso_tx_pool_alloc) >= HCI_ISO_BUF_ALLOC_PER_CHAN) {
+		if (!hci_wrn_printed) {
+			LOG_WRN("HCI ISO TX overrun");
+			hci_wrn_printed = true;
+		}
+		return;
+	}
+
+	hci_wrn_printed = false;
+
+	buf = net_buf_alloc(&tx_pool, K_FOREVER);
+	net_buf_reserve(buf, BT_ISO_CHAN_SEND_RESERVE);
+
+	net_buf_add_mem(buf, buf_data, len_to_send);
+
+	atomic_inc(&iso_tx_pool_alloc);
+	ret = bt_iso_chan_send(&iso_chan, buf, seq_num++, BT_ISO_TIMESTAMP_NONE);
+
+	if (ret < 0) {
+		LOG_INF("Failed to send ISO data (%d)", ret);
+		net_buf_unref(buf);
+		atomic_dec(&iso_tx_pool_alloc);
+	}
+
+	k_work_schedule(&iso_send_work, K_MSEC(5));
+}
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
@@ -29,11 +82,11 @@ static void connected(struct bt_conn *conn, uint8_t err)
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	if (err) {
-		printk("Failed to connect to %s (%u)\n", addr, err);
+		LOG_INF("Failed to connect to %s (%u)", addr, err);
 		return;
 	}
 
-	printk("Connected %s\n", addr);
+	LOG_INF("Connected %s", addr);
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
@@ -42,7 +95,7 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Disconnected from %s (reason 0x%02x)\n", addr, reason);
+	LOG_INF("Disconnected from %s (reason 0x%02x)", addr, reason);
 }
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
@@ -50,75 +103,44 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.disconnected = disconnected,
 };
 
-/** Print data as d_0 d_1 d_2 ... d_(n-2) d_(n-1) d_(n) to show the 3 first and 3 last octets
- *
- * Examples:
- * 01
- * 0102
- * 010203
- * 01020304
- * 0102030405
- * 010203040506
- * 010203...050607
- * 010203...060708
- * etc.
- */
-static void iso_print_data(uint8_t *data, size_t data_len)
-{
-	/* Maximum number of octets from each end of the data */
-	const uint8_t max_octets = 3;
-	char data_str[35];
-	size_t str_len;
-
-	str_len = bin2hex(data, MIN(max_octets, data_len), data_str, sizeof(data_str));
-	if (data_len > max_octets) {
-		if (data_len > (max_octets * 2)) {
-			static const char dots[] = "...";
-
-			strcat(&data_str[str_len], dots);
-			str_len += strlen(dots);
-		}
-
-		str_len += bin2hex(data + (data_len - MIN(max_octets, data_len - max_octets)),
-				   MIN(max_octets, data_len - max_octets),
-				   data_str + str_len,
-				   sizeof(data_str) - str_len);
-	}
-
-	printk("\t %s\n", data_str);
-}
-
 static void iso_recv(struct bt_iso_chan *chan, const struct bt_iso_recv_info *info,
 		struct net_buf *buf)
 {
-	printk("Incoming data channel %p len %u\n", chan, buf->len);
-	iso_print_data(buf->data, buf->len);
+	LOG_INF("Incoming data channel %p len %u", chan, buf->len);
 }
 
 static void iso_connected(struct bt_iso_chan *chan)
 {
-	printk("ISO Channel %p connected\n", chan);
+	LOG_INF("ISO Channel %p connected", chan);
+	k_work_reschedule(&iso_send_work, K_MSEC(100));
 }
 
 static void iso_disconnected(struct bt_iso_chan *chan, uint8_t reason)
 {
-	printk("ISO Channel %p disconnected (reason 0x%02x)\n", chan, reason);
+	LOG_INF("ISO Channel %p disconnected (reason 0x%02x)", chan, reason);
+	k_work_cancel_delayable(&iso_send_work);
+	atomic_clear(&iso_tx_pool_alloc);
 }
 
+static void iso_sent(struct bt_iso_chan *chan)
+{
+	atomic_dec(&iso_tx_pool_alloc);
+}
 static struct bt_iso_chan_ops iso_ops = {
 	.recv		= iso_recv,
 	.connected	= iso_connected,
 	.disconnected	= iso_disconnected,
+	.sent = iso_sent,
 };
 
-static struct bt_iso_chan_io_qos iso_rx = {
-	.sdu = CONFIG_BT_ISO_TX_MTU,
+static struct bt_iso_chan_io_qos iso_tx = {
+	//.sdu = CONFIG_BT_ISO_TX_MTU,
 	.path = NULL,
 };
 
 static struct bt_iso_chan_qos iso_qos = {
-	.rx = &iso_rx,
-	.tx = NULL,
+	.rx = NULL,
+	.tx = &iso_tx,
 };
 
 static struct bt_iso_chan iso_chan = {
@@ -129,10 +151,10 @@ static struct bt_iso_chan iso_chan = {
 static int iso_accept(const struct bt_iso_accept_info *info,
 		      struct bt_iso_chan **chan)
 {
-	printk("Incoming request from %p\n", (void *)info->acl);
+	LOG_INF("Incoming request from %p", (void *)info->acl);
 
 	if (iso_chan.iso) {
-		printk("No channels available\n");
+		LOG_INF("No channels available");
 		return -ENOMEM;
 	}
 
@@ -154,7 +176,7 @@ void main(void)
 
 	err = bt_enable(NULL);
 	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+		LOG_INF("Bluetooth init failed (err %d)", err);
 		return;
 	}
 
@@ -162,19 +184,20 @@ void main(void)
 		settings_load();
 	}
 
-	printk("Bluetooth initialized\n");
+	LOG_INF("Bluetooth initialized");
 
 	err = bt_iso_server_register(&iso_server);
 	if (err) {
-		printk("Unable to register ISO server (err %d)\n", err);
+		LOG_INF("Unable to register ISO server (err %d)", err);
 		return;
 	}
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
+		LOG_INF("Advertising failed to start (err %d)", err);
 		return;
 	}
 
-	printk("Advertising successfully started\n");
+	LOG_INF("Advertising successfully started");
+	k_work_init_delayable(&iso_send_work, iso_timer_timeout);
 }
