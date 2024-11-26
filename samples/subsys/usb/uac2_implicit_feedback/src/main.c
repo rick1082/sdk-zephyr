6bc7bd2550603f2520003e56b3d8a2dd8741e975
Jui-Chou Chung <jui-chou.chung@nordicsemi.no>
2024-11-26 15:17:27 +0800

    update the buffer declare

--- samples/subsys/usb/uac2_implicit_feedback/src/main.c
+++ samples/subsys/usb/uac2_implicit_feedback/src/main.c
@@ -167,22 +167,20 @@ static void uac2_data_recv_cb(const struct device *dev, uint8_t terminal,
 static void uac2_buf_release_cb(const struct device *dev, uint8_t terminal,
 				void *buf, void *user_data)
 {
-	if (terminal == MICROPHONE_IN_TERMINAL_ID) {
-		k_mem_slab_free(&i2s_rx_slab, buf);
-	}
+
 }
 
- static uint8_t __aligned(UDC_BUF_ALIGN) data_buffer[24] = {0};
+static uint8_t __aligned(UDC_BUF_ALIGN) data_buffer[ROUND_UP(12, UDC_BUF_GRANULARITY)] = {0};
 static void uac2_sof(const struct device *dev, void *user_data)
 {
+	int ret;
 	ARG_UNUSED(dev);
 	struct usb_i2s_ctx *ctx = user_data;
 
-	if (ctx->i2s_started) {
-		feedback_process(ctx->fb);
-	}
-	if (usbd_uac2_send(dev, MICROPHONE_IN_TERMINAL_ID,
-			   data_buffer, ROUND_UP(data_buffer, UDC_BUF_GRANULARITY)) < 0) {
+	ret = usbd_uac2_send(dev, MICROPHONE_IN_TERMINAL_ID,
+			   data_buffer, ROUND_UP(data_buffer, UDC_BUF_GRANULARITY));
+	if (ret < 0) {
+		LOG_ERR("Failed to send data to host, ret = %d", ret);
 	}
 	return ;
 }
